c5518ab7416025ef6c59a8678e1a7a272272ee28
unknown <jaeman7@gmail.com>
2024-09-08 12:10:17 +0900

    js/config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_JGjnADlAS',
+        userPoolClientId: '4o6hiqikl4vv8j4hibhetnfa63',
+        region: 'ap-northeast-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
